c0aeb10effe339c4d95f17f1e27e18ef4f8f4f2d
Chiu, Yung-Hsiang <mirror.tw@gmail.com>
2018-04-27 15:11:01 +0800

    Fixed the service cannot invoke more than one time

--- app/src/main/java/com/mattfenlon/ghost/MainActivity.java
+++ app/src/main/java/com/mattfenlon/ghost/MainActivity.java
@@ -6,32 +6,34 @@ import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  final static String TAG = "ALERT-TEST";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     if (Settings.canDrawOverlays(this)) {
-
+      Log.i(TAG, "[OC]draw overlays");
       // Launch service right away - the user has already previously granted permission
       launchMainService();
     }
     else {
-
+      Log.i(TAG, "[OC]cannot draw overlays");
       // Check that the user has granted permission, and prompt them if not
       checkDrawOverlayPermission();
     }
   }
 
   private void launchMainService() {
-
+    Log.i(TAG, "[LMS]startService");
     Intent svc = new Intent(this, MainService.class);
-
     startService(svc);
-
+    Log.i(TAG, "[LMS]finish");
     finish();
   }
 
@@ -41,10 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Checks if app already has permission to draw overlays
     if (!Settings.canDrawOverlays(this)) {
-
+      Log.i(TAG, "[CDOP]request overlay permission");
       // If not, form up an Intent to launch the permission request
       Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-
       // Launch Intent, with the supplied request code
       startActivityForResult(intent, REQUEST_CODE);
     }
@@ -52,18 +53,18 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
-
+    Log.i(TAG, "[OAR]reqcode" + requestCode + " rescode=" + resultCode + " data=" + data);
     // Check if a request code is received that matches that which we provided for the overlay draw request
     if (requestCode == REQUEST_CODE) {
-
+      boolean res = Settings.canDrawOverlays(this);
       // Double-check that the user granted it, and didn't just dismiss the request
-      if (Settings.canDrawOverlays(this)) {
-
+      Log.i(TAG, "[OAR]canDrawOverlays=" + res);
+      if (res) {
+        Log.i(TAG, "[OAR]canDrawOverlays");
         // Launch the service
         launchMainService();
       }
       else {
-
         Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show();
       }
     }
--- app/src/main/java/com/mattfenlon/ghost/MainService.java
+++ app/src/main/java/com/mattfenlon/ghost/MainService.java
@@ -19,94 +19,86 @@ import android.widget.FrameLayout;
  */
 
 public class MainService extends Service implements View.OnTouchListener {
+    private static String TAG = MainService.class.getSimpleName();
+    private WindowManager windowManager;
+    private View floatyView;
 
-  private static final String TAG = MainService.class.getSimpleName();
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "[OB]");
+        return null;
+    }
 
-  private WindowManager windowManager;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "[OC]");
+        windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        addOverlayView();
+    }
 
-  private View floatyView;
-
-  @Override
-  public IBinder onBind(Intent intent) {
-
-    return null;
-  }
-
-  @Override
-  public void onCreate() {
-
-    super.onCreate();
-
-    windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-
-    addOverlayView();
-  }
-
-  private void addOverlayView() {
-
-    final WindowManager.LayoutParams params =
+    private void addOverlayView() {
+        final WindowManager.LayoutParams params =
             new WindowManager.LayoutParams(
                     WindowManager.LayoutParams.MATCH_PARENT,
                     WindowManager.LayoutParams.WRAP_CONTENT,
                     WindowManager.LayoutParams.TYPE_PHONE,
                     0,
                     PixelFormat.TRANSLUCENT);
-
-    params.gravity = Gravity.CENTER | Gravity.START;
-    params.x = 0;
-    params.y = 0;
-
-    FrameLayout interceptorLayout = new FrameLayout(this) {
-
-      @Override
-      public boolean dispatchKeyEvent(KeyEvent event) {
-
-        // Only fire on the ACTION_DOWN event, or you'll get two events (one for _DOWN, one for _UP)
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-
-          // Check if the HOME button is pressed
-          if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-
-            Log.v(TAG, "BACK Button Pressed");
-
-            // As we've taken action, we'll return true to prevent other apps from consuming the event as well
-            return true;
-          }
-        }
-
-        // Otherwise don't intercept the event
-        return super.dispatchKeyEvent(event);
-      }
-    };
-
-    floatyView = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.floating_view, interceptorLayout);
-
-    floatyView.setOnTouchListener(this);
-
-    windowManager.addView(floatyView, params);
-  }
-
-  @Override
-  public void onDestroy() {
-
-    super.onDestroy();
-
-    if (floatyView != null) {
-
-      windowManager.removeView(floatyView);
-
-      floatyView = null;
+        params.gravity = Gravity.CENTER | Gravity.START;
+        params.x = 0;
+        params.y = 0;
+        FrameLayout interceptorLayout = new FrameLayout(this) {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+                // Only fire on the ACTION_DOWN event, or you'll get two events (one for _DOWN, one for _UP)
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    // Check if the HOME button is pressed
+                    if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+                        Log.v(TAG, "BACK Button Pressed");
+                        // As we've taken action, we'll return true to prevent other apps from consuming the event as well
+                        closeService();
+                        return true;
+                    }
+                }
+                // Otherwise don't intercept the event
+                return super.dispatchKeyEvent(event);
+            }
+        };
+        floatyView = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.floating_view, interceptorLayout);
+        floatyView.setOnTouchListener(this);
+        Log.v(TAG, "[AOV] wm.addView");
+        windowManager.addView(floatyView, params);
     }
-  }
 
-  @Override
-  public boolean onTouch(View view, MotionEvent motionEvent) {
+    /*
+    @Override
+    public void onDestroy() {
+        if (floatyView != null) {
+            Log.v(TAG, "[OD] wm.removeView");
+            windowManager.removeView(floatyView);
+            floatyView = null;
+        }
+        super.onDestroy();
+    }*/
 
-    Log.v(TAG, "onTouch...");
+    private void closeService() {
+        synchronized (this) {
+            if (floatyView != null) {
+                Log.v(TAG, "[OD] wm.removeView");
+                windowManager.removeView(floatyView);
+                floatyView = null;
+            }
+            stopSelf();
+        }
+    }
 
-    // Kill service
-    onDestroy();
-
-    return true;
-  }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        Log.v(TAG, "onTouch...");
+        // Kill service
+        //onDestroy();
+        closeService();
+        return true;
+    }
 }

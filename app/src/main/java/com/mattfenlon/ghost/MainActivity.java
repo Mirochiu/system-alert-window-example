19e54bd44aabc592f9cb3f1666cad668f4ca4373
yhchiu <mirochiu@cht.com.tw>
2019-01-03 11:55:40 +0800

    Added workaround for the overlay setting page not handled key event

--- app/src/main/java/com/mattfenlon/ghost/MainActivity.java
+++ app/src/main/java/com/mattfenlon/ghost/MainActivity.java
@@ -11,24 +11,22 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    final static String TAG = "ALERT-TEST";
 
-  final static String TAG = "ALERT-TEST";
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    if (Settings.canDrawOverlays(this)) {
-      Log.i(TAG, "[OC]draw overlays");
-      // Launch service right away - the user has already previously granted permission
-      launchMainService();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Settings.canDrawOverlays(this)) {
+            Log.i(TAG, "[OC]draw overlays");
+            // Launch service right away - the user has already previously granted permission
+            launchMainService();
+        }
+        else {
+            Log.i(TAG, "[OC]cannot draw overlays");
+            // Check that the user has granted permission, and prompt them if not
+            checkDrawOverlayPermission();
+        }
     }
-    else {
-      Log.i(TAG, "[OC]cannot draw overlays");
-      // Check that the user has granted permission, and prompt them if not
-      checkDrawOverlayPermission();
-    }
-  }
 
     private void launchMainService() {
         // test adb commands
@@ -56,35 +54,40 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-  public final static int REQUEST_CODE = 10101;
+    public final static int REQUEST_CODE = 10101;
 
-  public void checkDrawOverlayPermission() {
-    // Checks if app already has permission to draw overlays
-    if (!Settings.canDrawOverlays(this)) {
-      Log.i(TAG, "[CDOP]request overlay permission");
-      // If not, form up an Intent to launch the permission request
-      Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-      // Launch Intent, with the supplied request code
-      startActivityForResult(intent, REQUEST_CODE);
+    public void checkDrawOverlayPermission() {
+        // Checks if app already has permission to draw overlays
+        if (!Settings.canDrawOverlays(this)) {
+            Log.i(TAG, "[CDOP]request overlay permission");
+            // If not, form up an Intent to launch the permission request
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            // Workaround: put extras for the key event not handled in Overlay Setting page.
+            intent.putExtra("extra_prefs_show_button_bar", true); // show the status bar
+            intent.putExtra("extra_prefs_show_skip", false); // skip button is gone
+            intent.putExtra("extra_prefs_set_next_text", ""); // next button is gone
+            intent.putExtra("extra_prefs_set_back_text", getString(R.string.txtCancel));
+            // Launch Intent, with the supplied request code
+            startActivityForResult(intent, REQUEST_CODE);
+        }
     }
-  }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
-    Log.i(TAG, "[OAR]reqcode" + requestCode + " rescode=" + resultCode + " data=" + data);
-    // Check if a request code is received that matches that which we provided for the overlay draw request
-    if (requestCode == REQUEST_CODE) {
-      boolean res = Settings.canDrawOverlays(this);
-      // Double-check that the user granted it, and didn't just dismiss the request
-      Log.i(TAG, "[OAR]canDrawOverlays=" + res);
-      if (res) {
-        Log.i(TAG, "[OAR]canDrawOverlays");
-        // Launch the service
-        launchMainService();
-      }
-      else {
-        Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show();
-      }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        Log.i(TAG, "[OAR]reqcode" + requestCode + " rescode=" + resultCode + " data=" + data);
+        // Check if a request code is received that matches that which we provided for the overlay draw request
+        if (requestCode == REQUEST_CODE) {
+            boolean res = Settings.canDrawOverlays(this);
+            // Double-check that the user granted it, and didn't just dismiss the request
+            Log.i(TAG, "[OAR]canDrawOverlays=" + res);
+            if (res) {
+                Log.i(TAG, "[OAR]canDrawOverlays");
+                // Launch the service
+                launchMainService();
+            }
+            else {
+                Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-  }
 }

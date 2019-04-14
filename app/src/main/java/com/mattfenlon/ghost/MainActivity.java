4ee5fb07b473ffda2d769896b2b2136744897355
Chiu, Yung-Hsiang <mirror.tw@gmail.com>
2019-04-14 13:29:50 +0800

    Add camera preview and refatory permission check

--- app/src/main/java/com/mattfenlon/ghost/MainActivity.java
+++ app/src/main/java/com/mattfenlon/ghost/MainActivity.java
@@ -1,35 +1,160 @@
 package com.mattfenlon.ghost;
 
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     final static String TAG = "ALERT-TEST";
 
+    public final static int PERMISSIONS_REQUEST_CODE1 = 10101;
+    public final static int PERMISSIONS_REQUEST_CODE2 = 10102;
+
+    private static final String[] PERMISSIONS_LIST = new String[]{
+            Manifest.permission.CAMERA
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-                Log.i(TAG, "[OC]no need overlays");
-            } else {
-                Log.i(TAG, "[OC]draw overlays");
-            }
-            // Launch service right away - the user has already previously granted permission
-            launchMainService();
+        if (!checkPermStage1()) {
+            requestPermStage1();
+            return;
         }
-        else {
-            Log.i(TAG, "[OC]cannot draw overlays");
-            // Check that the user has granted permission, and prompt them if not
-            checkDrawOverlayPermission();
+        if (!checkPermStage2()) {
+            requestPermStage2();
+            return;
+        }
+        launchMainService();
+    }
+
+    private boolean checkPermStage1() {
+        boolean allowAll = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<Integer> resList = new ArrayList();
+            for (String perm : PERMISSIONS_LIST) {
+                resList.add(checkSelfPermission(perm));
+            }
+            for (int res : resList) {
+                if (PackageManager.PERMISSION_GRANTED != res) {
+                    allowAll = false;
+                    break;
+                }
+            }
+            Log.v(TAG, "[CPS1] has all danger permission?" + allowAll);
+        } else {
+            Log.i(TAG, "[CPS1] no danger permission");
+        }
+        return allowAll;
+    }
+
+    private void requestPermStage1() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(PERMISSIONS_LIST, PERMISSIONS_REQUEST_CODE1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE1: {
+                boolean allowAll = true;
+                if (grantResults.length <= 0) return;
+                for (int res : grantResults) {
+                    if (PackageManager.PERMISSION_GRANTED != res) {
+                        allowAll = false;
+                        break;
+                    }
+                }
+                if (!allowAll) {
+                    showMsg("User now allowed to get all permission");
+                } else {
+                    if (!checkPermStage2()) {
+                        requestPermStage2();
+                    } else {
+                        launchMainService();
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean checkPermStage2() {
+        boolean allowAll = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            allowAll = Settings.canDrawOverlays(getApplicationContext());
+            Log.i(TAG, "[CPS2] has draw overlays");
+        } else {
+            Log.i(TAG, "[CPS2] no need overlays");
+        }
+        return allowAll;
+    }
+
+    public static boolean isActivityIntent(Context context, Intent intent) {
+        PackageManager pkgManager = context.getPackageManager();
+        List<ResolveInfo> list = pkgManager.queryIntentActivities(intent, 0);
+        if (null != list && list.size() >= 1) {
+            return true;
+        }
+        Log.v(TAG, "Not found Activity");
+        return false;
+    }
+
+    private void requestPermStage2() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Log.i(TAG, "[RPS2] request overlay permission");
+                // If not, form up an Intent to launch the permission request
+                Intent intent = new Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                // Workaround: put extras for the key event not handled in Overlay Setting page.
+                intent.putExtra("extra_prefs_show_button_bar", true); // show the status bar
+                intent.putExtra("extra_prefs_show_skip", false); // skip button is gone
+                intent.putExtra("extra_prefs_set_next_text", ""); // next button is gone
+                intent.putExtra("extra_prefs_set_back_text", getString(R.string.txtCancel));
+                // Launch Intent, with the supplied request code
+                if (isActivityIntent(getApplicationContext(), intent)) {
+                    startActivityForResult(intent, PERMISSIONS_REQUEST_CODE2);
+                } else {
+                    showMsg("ERROR:Cannot found overlay permission request activity");
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        Log.i(TAG, "[OAR]reqcode" + requestCode + " rescode=" + resultCode + " data=" + data);
+        // Check if a request code is received that matches that which we provided for the overlay draw request
+        if (PERMISSIONS_REQUEST_CODE2 == requestCode) {
+            boolean res = checkPermStage2();
+            // Double-check that the user granted it, and didn't just dismiss the request
+            Log.i(TAG, "[OAR]canDrawOverlays=" + res);
+            if (res) {
+                Log.i(TAG, "[OAR]canDrawOverlays");
+                // Launch the service
+                launchMainService();
+            }
+            else {
+                showMsg("Sorry. Can't draw overlays without permission...");
+            }
         }
     }
 
@@ -48,10 +173,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "[LMS]startService");
         Intent service = new Intent(this, MainService.class);
         //service.putExtra("show-message", "launchMainService " + svc.hashCode());
-        service.putExtra("play-movie-url", "<null>");
+        //service.putExtra("play-movie-url", "<null>");
         //service.putExtra("play-music-url", "<null>");
         //service.putExtra("show-message-and-ringtone", "ringtone message");
         //service.putExtra("play-movie-url", "udp://230.1.2.108:11111/");
+        service.putExtra("play-movie-url-and-camera-preview", "<null>");
         ContextCompat.startForegroundService(getApplicationContext(), service);
         /*
         // https://stackoverflow.com/questions/46445265/android-8-0-java-lang-illegalstateexception-not-allowed-to-start-service-inten
@@ -65,46 +191,14 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    public final static int REQUEST_CODE = 10101;
-
-    public void checkDrawOverlayPermission() {
-        // Checks if app already has permission to draw overlays
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            Log.i(TAG, "[CDOP]request overlay permission");
-            // If not, form up an Intent to launch the permission request
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-            // Workaround: put extras for the key event not handled in Overlay Setting page.
-            intent.putExtra("extra_prefs_show_button_bar", true); // show the status bar
-            intent.putExtra("extra_prefs_show_skip", false); // skip button is gone
-            intent.putExtra("extra_prefs_set_next_text", ""); // next button is gone
-            intent.putExtra("extra_prefs_set_back_text", getString(R.string.txtCancel));
-            // Launch Intent, with the supplied request code
-            startActivityForResult(intent, REQUEST_CODE);
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                Log.d(TAG, "check overlay permission okay!");
-            } else {
-                Log.d(TAG, "No need Overlays");
+    private void showMsg(final String msg) {
+        try {
+            if (!TextUtils.isEmpty(msg)) {
+                Log.i(TAG, msg);
+                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
             }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
-        Log.i(TAG, "[OAR]reqcode" + requestCode + " rescode=" + resultCode + " data=" + data);
-        // Check if a request code is received that matches that which we provided for the overlay draw request
-        if (requestCode == REQUEST_CODE) {
-            boolean res = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this);
-            // Double-check that the user granted it, and didn't just dismiss the request
-            Log.i(TAG, "[OAR]canDrawOverlays=" + res);
-            if (res) {
-                Log.i(TAG, "[OAR]canDrawOverlays");
-                // Launch the service
-                launchMainService();
-            }
-            else {
-                Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show();
-            }
+        } catch (Exception e) {
+            // nothing
         }
     }
 }
